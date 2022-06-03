ab9caf03c3465f321e8bcd3795e7f7e681e718f8
Patrick Joseph Magbuhos <patrickjosephmagbuhos@gmail.com>
2022-06-03 13:18:01 +0800

    Buttons Added

--- Tic Tac Toe/Form1.Designer.cs
+++ Tic Tac Toe/Form1.Designer.cs
@@ -29,13 +29,120 @@ namespace Tic_Tac_Toe
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Tile1 = new System.Windows.Forms.Button();
+            this.Tile2 = new System.Windows.Forms.Button();
+            this.Tile3 = new System.Windows.Forms.Button();
+            this.Tile4 = new System.Windows.Forms.Button();
+            this.Tile5 = new System.Windows.Forms.Button();
+            this.Tile6 = new System.Windows.Forms.Button();
+            this.Tile7 = new System.Windows.Forms.Button();
+            this.Tile8 = new System.Windows.Forms.Button();
+            this.Tile9 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Tile1
+            // 
+            this.Tile1.Location = new System.Drawing.Point(33, 68);
+            this.Tile1.Name = "Tile1";
+            this.Tile1.Size = new System.Drawing.Size(109, 77);
+            this.Tile1.TabIndex = 0;
+            this.Tile1.UseVisualStyleBackColor = true;
+            // 
+            // Tile2
+            // 
+            this.Tile2.Location = new System.Drawing.Point(33, 151);
+            this.Tile2.Name = "Tile2";
+            this.Tile2.Size = new System.Drawing.Size(109, 77);
+            this.Tile2.TabIndex = 1;
+            this.Tile2.UseVisualStyleBackColor = true;
+            // 
+            // Tile3
+            // 
+            this.Tile3.Location = new System.Drawing.Point(33, 234);
+            this.Tile3.Name = "Tile3";
+            this.Tile3.Size = new System.Drawing.Size(109, 77);
+            this.Tile3.TabIndex = 2;
+            this.Tile3.UseVisualStyleBackColor = true;
+            // 
+            // Tile4
+            // 
+            this.Tile4.Location = new System.Drawing.Point(148, 234);
+            this.Tile4.Name = "Tile4";
+            this.Tile4.Size = new System.Drawing.Size(109, 77);
+            this.Tile4.TabIndex = 5;
+            this.Tile4.UseVisualStyleBackColor = true;
+            // 
+            // Tile5
+            // 
+            this.Tile5.Location = new System.Drawing.Point(148, 151);
+            this.Tile5.Name = "Tile5";
+            this.Tile5.Size = new System.Drawing.Size(109, 77);
+            this.Tile5.TabIndex = 4;
+            this.Tile5.UseVisualStyleBackColor = true;
+            // 
+            // Tile6
+            // 
+            this.Tile6.Location = new System.Drawing.Point(148, 68);
+            this.Tile6.Name = "Tile6";
+            this.Tile6.Size = new System.Drawing.Size(109, 77);
+            this.Tile6.TabIndex = 3;
+            this.Tile6.UseVisualStyleBackColor = true;
+            // 
+            // Tile7
+            // 
+            this.Tile7.Location = new System.Drawing.Point(263, 234);
+            this.Tile7.Name = "Tile7";
+            this.Tile7.Size = new System.Drawing.Size(109, 77);
+            this.Tile7.TabIndex = 8;
+            this.Tile7.UseVisualStyleBackColor = true;
+            // 
+            // Tile8
+            // 
+            this.Tile8.Location = new System.Drawing.Point(263, 151);
+            this.Tile8.Name = "Tile8";
+            this.Tile8.Size = new System.Drawing.Size(109, 77);
+            this.Tile8.TabIndex = 7;
+            this.Tile8.UseVisualStyleBackColor = true;
+            // 
+            // Tile9
+            // 
+            this.Tile9.Location = new System.Drawing.Point(263, 68);
+            this.Tile9.Name = "Tile9";
+            this.Tile9.Size = new System.Drawing.Size(109, 77);
+            this.Tile9.TabIndex = 6;
+            this.Tile9.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(404, 324);
+            this.Controls.Add(this.Tile7);
+            this.Controls.Add(this.Tile8);
+            this.Controls.Add(this.Tile9);
+            this.Controls.Add(this.Tile4);
+            this.Controls.Add(this.Tile5);
+            this.Controls.Add(this.Tile6);
+            this.Controls.Add(this.Tile3);
+            this.Controls.Add(this.Tile2);
+            this.Controls.Add(this.Tile1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button Tile1;
+        private System.Windows.Forms.Button Tile2;
+        private System.Windows.Forms.Button Tile3;
+        private System.Windows.Forms.Button Tile4;
+        private System.Windows.Forms.Button Tile5;
+        private System.Windows.Forms.Button Tile6;
+        private System.Windows.Forms.Button Tile7;
+        private System.Windows.Forms.Button Tile8;
+        private System.Windows.Forms.Button Tile9;
     }
 }
 
